226df3d2d3e9986788886594491f34a3c8581c11
MichaelYeh507 <54426940+MichaelYeh507@users.noreply.github.com>
2020-08-24 16:57:45 -0700

    linearSearch

--- src/_01_Searching_Algorithms/_00_LinearSearch.java
+++ src/_01_Searching_Algorithms/_00_LinearSearch.java
@@ -9,7 +9,13 @@ public class _00_LinearSearch {
 		//   in the array. Return the location in the array
 		//   where the value was found.
 		//   If the value is not found in the array, return -1.
-		
+		boolean found = false;
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].equals(value)) {
+				found = true;
+				return i;
+			}
+		}
 		return 0;
 	}
 }
